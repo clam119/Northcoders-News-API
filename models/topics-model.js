aee7c241c6652c2475b70e2e59043bf45923514c
clam119 <christopher.lam119@gmail.com>
2022-10-10 14:18:40 +0100

    Remove unnecessary console log

--- models/topics-model.js
+++ models/topics-model.js
@@ -4,7 +4,6 @@ exports.fetchAllTopics = () => {
     return db
     .query(`SELECT * FROM topics;`)
     .then(({rows: topicsData}) => {
-        console.log(topicsData);
         return topicsData;
     })
 }
