6c2120d61edd20d393888cb9a5762fe5701a63a7
clam119 <christopher.lam119@gmail.com>
2022-10-10 17:26:57 +0100

    Move the promise rejection with the custom status & message over into the conditional statement. Then just return a Promise.reject that contains the error in the catch block of the articles model.

--- models/articles-model.js
+++ models/articles-model.js
@@ -5,17 +5,13 @@ exports.fetchArticleByID = (article_id) => {
     .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
     .then(({rows: articleData}) => {
         if(articleData.length === 0) {
-            throw new Error;
+            return Promise.reject({status: 404, msg: 'That article does not exist'});
         }
         else {
             return articleData;
         }
     })
     .catch((err) => {
-       if(err.code === '22P02') {
-        return Promise.reject({status: 400, msg: 'Invalid Data Type'})
-       } 
-       
-       return Promise.reject({status: 404, msg: 'That article does not exist'})
+       return Promise.reject(err)
     })
 }
