683327b08454f4aee2a69f7cf3c588a8a9df1d95
clam119 <christopher.lam119@gmail.com>
2022-10-11 13:57:34 +0100

    Initial Commit - Add a separate JS file that will keep track of the error handling middleware functions - handleCustomErrors, handlePsqlErrors and handle500Errors

---
+++ errors.js
@@ -0,0 +1,23 @@
+exports.handleCustomErrors = (err, req, res, next) => {
+    if(err.status && err.msg) {
+        res.status(err.status).send(err.msg);
+    }
+    else {
+        next(err);
+    }
+}
+
+exports.handlePsqLErrors = (err, req, res, next) => {
+    if(err.code === '22P02') {
+      res.status(400).send('Invalid Data Type');
+    } 
+    else {
+        next(err);
+    }
+}
+
+exports.handle500Errors = (err, req, res, next) => {
+    console.log('A custom 500 Error has occurred, check in errors.js', err);
+    res.status(500).send({msg: 'Internal Server Error - We will look into this'})
+}
+
