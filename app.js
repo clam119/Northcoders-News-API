3262de4014b8c8ebd23426db1f03e05bf8470830
clam119 <christopher.lam119@gmail.com>
2022-10-13 12:13:40 +0100

    Add the postCommentByArticleID controller function and create the endpoint to post comments to specific articles by their ID.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { getAllTopics } = require('./controllers/topics-controller');
 const { getAllUsers } = require('./controllers/users-controller');
-const { getArticleByID, patchArticleByID, getAllArticles, getCommentsByID } = require('./controllers/articles-controller');
+const { getArticleByID, patchArticleByID, getAllArticles, getCommentsByID, postCommentByArticleID } = require('./controllers/articles-controller');
 const { handleCustomErrors, handlePsqLErrors, handle500Errors } = require('./errors');
 const express = require('express');
 const app = express();
@@ -24,6 +24,9 @@ app.get('/api/articles', getAllArticles)
 
 // Task 7 - 'GET /api/articles/:article_id/comments
 app.get('/api/articles/:article_id/comments', getCommentsByID);
+
+// Task 8 - 'POST /api/articles/:article_id/comments
+app.post('/api/articles/:article_id/comments', postCommentByArticleID);
 
 app.all('/*', (req, res, next) => { 
     res.status(404).send({msg: 'path not found'});
