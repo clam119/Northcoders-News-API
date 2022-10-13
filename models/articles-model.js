60b74c48f1620732a70481e2a5deeedc1f5f2fc7
clam119 <christopher.lam119@gmail.com>
2022-10-13 11:54:47 +0100

    Add separate query that will push all of the articles into an array and get the length of that array. This array will be used to check if an article id is valid or not, by checking if the article id is greater than the amount of articles in the existingArticles array, if so it will return a custom 404 error.

--- models/articles-model.js
+++ models/articles-model.js
@@ -93,11 +93,18 @@ exports.updateArticleByID = (article_id, inc_votes) => {
 };
 
 exports.fetchCommentsByID = (article_id) => {
+  const existingArticles = [];
+
+  db.query(`SELECT article_id FROM articles`).then(({rows: articles}) => articles.forEach(article => existingArticles.push(article)));
+  
   return db
-  .query(`SELECT comments.* FROM comments LEFT JOIN articles ON comments.article_id = articles.article_id WHERE comments.article_id = $1 ORDER BY comments.created_at DESC;`, [article_id])
+  .query(`SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`, [article_id])
   .then(({rows: comments}) => {
-    if(comments.length === 0) {
-      return Promise.reject({status: 404, msg: "No comments found"});
+    if(article_id > existingArticles.length) {
+      return Promise.reject({status:404, msg: "Article not found"})
+    }
+    if(comments.length === 0 && article_id <= existingArticles.length) {
+      return Promise.reject({status: 200, msg: "No comments found for that article yet"});
     }
     else {
       return comments
