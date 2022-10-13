1a1b5e27633e76b8891f11de2a23a46990540e2f
clam119 <christopher.lam119@gmail.com>
2022-10-13 16:39:10 +0100

    Add conditional logic that will add by default 'created_at' as the default sort_by criteria and 'order' will be by default descending. If the user does pass in an order/sort_by query but they are not within the acceptable query parameters, then a 400 status code custom error will be sent back to the user.

--- models/articles-model.js
+++ models/articles-model.js
@@ -22,32 +22,77 @@ exports.fetchArticleByID = (article_id) => {
     });
 };
 
-exports.fetchAllArticles = (topic) => {
-  const acceptableQueries = [];
+exports.fetchAllArticles = (topic, sort_by, order) => {
+  const acceptableTopicQueries = [];
+  const acceptableOrderQueries = ["asc", "desc", "ASC", "DESC"];
+  const acceptableSortByQueries = [
+    "article_id",
+    "votes",
+    "ARTICLE_ID",
+    "VOTES",
+  ];
+
+  if (!acceptableSortByQueries.includes(sort_by) && sort_by !== undefined) {
+    return Promise.reject({status: 400, msg: "Invalid Data Type"})
+  }
+
+  if(!acceptableOrderQueries.includes(order) && order !== undefined) {
+    return Promise.reject({status: 400, msg: "Invalid Data Type"})
+  }
 
   let filteredQueryString = `SELECT articles.*, COUNT(comments.author) AS comment_count
     FROM articles
     LEFT JOIN comments
     ON articles.article_id = comments.article_id
     WHERE articles.topic = $1
-    GROUP BY articles.article_id 
-    ORDER BY articles.created_at DESC`;
+    GROUP BY articles.article_id `;
 
   let baseQueryString = `SELECT articles.*, COUNT(comments.author) AS comment_count
     FROM articles
     LEFT JOIN comments
     ON articles.article_id = comments.article_id
-    GROUP BY articles.article_id 
-    ORDER BY articles.created_at DESC`;
+    GROUP BY articles.article_id  `;
+
+  if (sort_by) {
+    filteredQueryString += `ORDER BY ${sort_by} `;
+    baseQueryString += ` ORDER BY ${sort_by} `;
+  }
+
+  if (order) {
+    filteredQueryString += order;
+    baseQueryString += order;
+  }
+
+  if (sort_by === undefined || sort_by.length === 0) {
+    sort_by = "created_at";
+    filteredQueryString += `ORDER BY ${sort_by} `;
+    baseQueryString += `ORDER BY ${sort_by} `;
+  }
+
+  if (order === undefined || order.length === 0) {
+    order = "DESC";
+    filteredQueryString += order;
+    baseQueryString += order;
+  }
+ 
 
   return db
     .query(`SELECT slug FROM topics`)
     .then(({ rows: topics }) =>
-      topics.forEach((topic) => acceptableQueries.push(topic.slug))
+      topics.forEach((topic) => acceptableTopicQueries.push(topic.slug))
     )
     .then(() => {
       //Condition 1: If topic isn't given by default then will regularly query
       //All articles in descending creation date order & reject 404 if no articles found
+
+      if(!acceptableSortByQueries.includes(sort_by) && sort_by === undefined) {
+        return Promise.reject({status: 400, msg: "Invalid Data Type"});
+      }
+
+      if(!acceptableOrderQueries.includes(order) && sort_by === undefined) {
+        return Promise.reject({status: 400, msg: "Invalid Data Type"});
+      }
+
       if (topic === undefined || topic.length === 0) {
         return db.query(baseQueryString).then(({ rows: allArticlesData }) => {
           if (allArticlesData.length === 0) {
@@ -59,7 +104,7 @@ exports.fetchAllArticles = (topic) => {
       }
 
       //Condition 2: If no topics queried & not within acceptableQueries, will return 404
-      if (!acceptableQueries.includes(topic) && topic.length !== 0) {
+      if (!acceptableTopicQueries.includes(topic) && topic.length !== 0) {
         return Promise.reject({
           status: 404,
           msg: "Article with that topic not found",
@@ -67,7 +112,7 @@ exports.fetchAllArticles = (topic) => {
       }
 
       //Condition 3: If acceptable query given then will return filteredArticles of specified topic
-      if (acceptableQueries.includes(topic) && topic.length > 1) {
+      if (acceptableTopicQueries.includes(topic) && topic.length > 1) {
         return db
           .query(filteredQueryString, [topic])
           .then(({ rows: filteredArticlesData }) => {
@@ -81,7 +126,10 @@ exports.updateArticleByID = (article_id, inc_votes) => {
   if (typeof inc_votes !== "number") {
     return Promise.reject({ status: 400, msg: "Invalid Data Type" });
   }
-  return db.query(`UPDATE articles SET votes = (votes + $1) WHERE article_id = $2 RETURNING *;`,[inc_votes, article_id]
+  return db
+    .query(
+      `UPDATE articles SET votes = (votes + $1) WHERE article_id = $2 RETURNING *;`,
+      [inc_votes, article_id]
     )
     .then(({ rows: [updatedArticleData] }) => {
       return updatedArticleData;
@@ -96,7 +144,10 @@ exports.fetchCommentsByID = (article_id) => {
   );
 
   return db
-    .query(`SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`,[article_id])
+    .query(
+      `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`,
+      [article_id]
+    )
     .then(({ rows: comments }) => {
       if (article_id > existingArticles.length) {
         return Promise.reject({ status: 404, msg: "Article not found" });
@@ -107,8 +158,11 @@ exports.fetchCommentsByID = (article_id) => {
 
 exports.createCommentByID = (article_id, username, body) => {
   return db
-  .query(`INSERT INTO comments(author, body, article_id) VALUES ($1, $2, $3) RETURNING *;`, [username, body, article_id])
-  .then(({rows: [postedComment]}) => {
+    .query(
+      `INSERT INTO comments(author, body, article_id) VALUES ($1, $2, $3) RETURNING *;`,
+      [username, body, article_id]
+    )
+    .then(({ rows: [postedComment] }) => {
       return postedComment;
-  })
-}
+    });
+};
