2840c1e933ca82d11c5800bd7a39b2f0cf3fab27
clam119 <christopher.lam119@gmail.com>
2022-10-13 20:39:32 +0100

    Completely refactored the entire fetchAllArticles using Async Await to reduced multiple queries & then chain.

--- models/articles-model.js
+++ models/articles-model.js
@@ -22,104 +22,39 @@ exports.fetchArticleByID = (article_id) => {
     });
 };
 
-exports.fetchAllArticles = (topic, sort_by, order) => {
-  const acceptableTopicQueries = [];
-  const acceptableOrderQueries = ["asc", "desc", "ASC", "DESC"];
-  const acceptableSortByQueries = [
+exports.fetchAllArticles = async (topic, sort_by = "created_at", order = "DESC") => {
+  let baseQuery = `SELECT articles.*, COUNT(comments.author) as comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id`;
+  const queryValues = [];
+  const validColumns = [
+    "author",
+    "title",
     "article_id",
+    "topic",
+    "created_at",
     "votes",
-    "ARTICLE_ID",
-    "VOTES",
+    "comment_count",
   ];
+  const validOrders = ["ASC", "DESC", "asc", "desc"];
 
-  if (!acceptableSortByQueries.includes(sort_by) && sort_by !== undefined) {
-    return Promise.reject({status: 400, msg: "Invalid Data Type"})
+  if (topic) {
+    baseQuery += ` WHERE articles.topic = $1`;
+    queryValues.push(topic);
+    const checkDb = await db.query(`SELECT * FROM topics WHERE slug = $1;`, queryValues)
+    if(checkDb.rows.length === 0) {
+      return Promise.reject({status: 404, msg: "Article with that topic not found"})
+    }
   }
 
-  if(!acceptableOrderQueries.includes(order) && order !== undefined) {
-    return Promise.reject({status: 400, msg: "Invalid Data Type"})
+  if (!validColumns.includes(sort_by) || !validOrders.includes(order)) {
+    return Promise.reject({ status: 400, msg: "Invalid Data Type" });
   }
 
-  let filteredQueryString = `SELECT articles.*, COUNT(comments.author) AS comment_count
-    FROM articles
-    LEFT JOIN comments
-    ON articles.article_id = comments.article_id
-    WHERE articles.topic = $1
-    GROUP BY articles.article_id `;
+  baseQuery += ` GROUP BY articles.article_id ORDER BY ${sort_by} ${order.toUpperCase()}`;
 
-  let baseQueryString = `SELECT articles.*, COUNT(comments.author) AS comment_count
-    FROM articles
-    LEFT JOIN comments
-    ON articles.article_id = comments.article_id
-    GROUP BY articles.article_id  `;
-
-  if (sort_by) {
-    filteredQueryString += `ORDER BY ${sort_by} `;
-    baseQueryString += ` ORDER BY ${sort_by} `;
-  }
-
-  if (order) {
-    filteredQueryString += order;
-    baseQueryString += order;
-  }
-
-  if (sort_by === undefined || sort_by.length === 0) {
-    sort_by = "created_at";
-    filteredQueryString += `ORDER BY ${sort_by} `;
-    baseQueryString += `ORDER BY ${sort_by} `;
-  }
-
-  if (order === undefined || order.length === 0) {
-    order = "DESC";
-    filteredQueryString += order;
-    baseQueryString += order;
-  }
- 
-
-  return db
-    .query(`SELECT slug FROM topics`)
-    .then(({ rows: topics }) =>
-      topics.forEach((topic) => acceptableTopicQueries.push(topic.slug))
-    )
-    .then(() => {
-      //Condition 1: If topic isn't given by default then will regularly query
-      //All articles in descending creation date order & reject 404 if no articles found
-
-      if(!acceptableSortByQueries.includes(sort_by) && sort_by === undefined) {
-        return Promise.reject({status: 400, msg: "Invalid Data Type"});
-      }
-
-      if(!acceptableOrderQueries.includes(order) && sort_by === undefined) {
-        return Promise.reject({status: 400, msg: "Invalid Data Type"});
-      }
-
-      if (topic === undefined || topic.length === 0) {
-        return db.query(baseQueryString).then(({ rows: allArticlesData }) => {
-          if (allArticlesData.length === 0) {
-            return Promise.reject({ status: 404, msg: "Article not found" });
-          } else {
-            return allArticlesData;
-          }
-        });
-      }
-
-      //Condition 2: If no topics queried & not within acceptableQueries, will return 404
-      if (!acceptableTopicQueries.includes(topic) && topic.length !== 0) {
-        return Promise.reject({
-          status: 404,
-          msg: "Article with that topic not found",
-        });
-      }
-
-      //Condition 3: If acceptable query given then will return filteredArticles of specified topic
-      if (acceptableTopicQueries.includes(topic) && topic.length > 1) {
-        return db
-          .query(filteredQueryString, [topic])
-          .then(({ rows: filteredArticlesData }) => {
-            return filteredArticlesData;
-          });
-      }
-    });
+  return db.query(baseQuery, queryValues).then(({ rows: articles }) => {
+    return articles;
+  });
+  
 };
 
 exports.updateArticleByID = (article_id, inc_votes) => {
