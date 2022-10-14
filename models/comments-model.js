9f868fec7f155a6ce0db4c47deb51dd9e0eb4103
clam119 <christopher.lam119@gmail.com>
2022-10-14 06:39:58 +0100

    Add the comments-model with the solution using async await syntax, will filter out with regex if the passed ID is a number or not & checks if the ID exists or not by comparing it to the length of the validCommentIDs array.

---
+++ models/comments-model.js
@@ -0,0 +1,21 @@
+const db = require("../db/connection");
+const { filter } = require("../db/data/test-data/articles");
+
+exports.deleteComment = async (comment_id) => {
+    const checkDbCommentID = await db.query(`SELECT comment_id FROM comments`)
+    const validCommentIDs = [...checkDbCommentID.rows]
+
+    let filterNums = /\d+/.test(comment_id)
+    
+    if(comment_id > validCommentIDs.length) {
+        return Promise.reject({ status: 404, msg: "Comment with that ID not found" });
+    }
+
+    if(!filterNums) {
+        return Promise.reject({ status: 400, msg: "Invalid Data Type" });
+    }
+
+   await db.query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id]);
+    
+    
+}
