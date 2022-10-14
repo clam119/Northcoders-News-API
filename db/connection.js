836920f6893b0d363448bec63c3cc9f91a100ce1
clam119 <christopher.lam119@gmail.com>
2022-10-14 12:38:01 +0100

    Add custom connection that will allow me to connect to the hosted database from localhost.

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,14 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+const config = ENV === 'production' ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -9,4 +17,4 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
