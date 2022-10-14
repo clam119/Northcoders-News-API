2723e5d34819c9ec68ec0beec2b60fd34b3a9339
clam119 <christopher.lam119@gmail.com>
2022-10-14 14:33:32 +0100

    Change App.js to use Express Router.

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
 const { getAvailableEndpoints } = require('./controllers/api-controller');
-const { getAllTopics } = require('./controllers/topics-controller');
-const { getAllUsers } = require('./controllers/users-controller');
-const { getArticleByID, patchArticleByID, getAllArticles, getCommentsByID, postCommentByArticleID } = require('./controllers/articles-controller');
-const { deleteCommentByID } = require('./controllers/comments-controller.js');
 const { handleCustomErrors, handlePsqLErrors, handle500Errors } = require('./errors');
 const express = require('express');
 const app = express();
+const apiRouter = require('./routes/api-router');
 
 app.use(express.json());
+app.use('/api', apiRouter);
 
 // '/api'
 app.get('/api', getAvailableEndpoints);
-
-// '/api/topics'
-app.get('/api/topics', getAllTopics);
-
-// '/api/users'
-app.get('/api/users', getAllUsers);
-
-// '/api/articles'
-app.get('/api/articles', getAllArticles)
-
-// '/api/articles/:article_id
-app.get('/api/articles/:article_id', getArticleByID);
-app.patch('/api/articles/:article_id', patchArticleByID);
-
-// '/api/articles/:article_id/comments
-app.get('/api/articles/:article_id/comments', getCommentsByID);
-app.post('/api/articles/:article_id/comments', postCommentByArticleID);
-app.delete('/api/comments/:comment_id', deleteCommentByID)
 
 app.all('/*', (req, res, next) => { 
     res.status(404).send({msg: 'path not found'});
