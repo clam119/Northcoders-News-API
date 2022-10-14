1e20041b70739d4379eac3975c04e2cac97d1727
clam119 <christopher.lam119@gmail.com>
2022-10-14 14:53:54 +0100

    Removed the app.get request in api-router.js that was causing it to not display all of the api endpoints when /api is accessed.

--- routes/api-router.js
+++ routes/api-router.js
@@ -4,10 +4,6 @@ const topicsRouter  = require('./topics-router');
 const articlesRouter = require('./articles-router');
 const commentsRouter = require('./comments-router');
 
-apiRouter.get('/', (req, res) => {
-    res.status(200).send('All OK from API Router');
-})
-
 apiRouter.use('/users', userRouter);
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/articles', articlesRouter);
