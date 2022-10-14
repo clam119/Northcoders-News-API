fefe90624494cf0b4e070c80d1d40a5ca4e78e30
clam119 <christopher.lam119@gmail.com>
2022-10-14 09:41:07 +0100

    Reformatted each endpoint to have its own section - will need to use routers later on to re-organise.

--- app.js
+++ app.js
@@ -9,32 +9,26 @@ const app = express();
 
 app.use(express.json());
 
-// Task 2 - 'GET /api/topics'
+// '/api'
+app.get('/api', getAvailableEndpoints);
+
+// '/api/topics'
 app.get('/api/topics', getAllTopics);
 
-// Task 3 - 'GET /api/articles/:article_id'
-app.get('/api/articles/:article_id', getArticleByID);
-
-// Task 4 - 'GET /api/users'
+// '/api/users'
 app.get('/api/users', getAllUsers);
 
-// Task 5 - 'PATCH /api/articles/:article_id
-app.patch('/api/articles/:article_id', patchArticleByID);
-
-//Task 6 - 'GET /api/articles'
+// '/api/articles'
 app.get('/api/articles', getAllArticles)
 
-// Task 7 - 'GET /api/articles/:article_id/comments
+// '/api/articles/:article_id
+app.get('/api/articles/:article_id', getArticleByID);
+app.patch('/api/articles/:article_id', patchArticleByID);
+
+// '/api/articles/:article_id/comments
 app.get('/api/articles/:article_id/comments', getCommentsByID);
-
-// Task 8 - 'POST /api/articles/:article_id/comments
 app.post('/api/articles/:article_id/comments', postCommentByArticleID);
-
-// Task 9 - 'DELETE /api/comments/:comment_id
 app.delete('/api/comments/:comment_id', deleteCommentByID)
-
-// Task 10 - 'GET /api'
-app.get('/api', getAvailableEndpoints);
 
 app.all('/*', (req, res, next) => { 
     res.status(404).send({msg: 'path not found'});
