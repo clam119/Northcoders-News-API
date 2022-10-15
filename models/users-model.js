afb88dbd1eceb2fb30571f164bcd313ed8ef7e7e
clam119 <christopher.lam119@gmail.com>
2022-10-15 04:42:51 +0100

    Add conditional logic to check if the given username exists in the database, if it doesn't exist then a 404 error is returned

--- models/users-model.js
+++ models/users-model.js
@@ -9,8 +9,15 @@ exports.fetchAllUsers = () => {
 }
 
 exports.fetchUserByUsername = async (username) => {
+    const checkDbUsernames = await db.query(`SELECT * FROM users`);
+    const validUsernames = await checkDbUsernames.rows.map((user) => user.username)
     const response = await db.query(`SELECT * FROM users WHERE username = $1`, [username]);
     const data = await response;
     const { rows: [userData] } = data;
+    
+    if(!validUsernames.includes(username)) {
+        return Promise.reject({status: 404, msg: "Username not found"})
+    }
+    
     return userData;
 }
