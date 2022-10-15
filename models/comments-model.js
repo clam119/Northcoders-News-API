923bbbc66f4ee5bdc3389b2630c9171d546495b1
clam119 <christopher.lam119@gmail.com>
2022-10-15 11:16:36 +0100

    Added the functionality that will increment/decrement a comment's vote count by the requested comment ID by the user.

--- models/comments-model.js
+++ models/comments-model.js
@@ -15,6 +15,14 @@ exports.deleteComment = async (comment_id) => {
     }
 
    await db.query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id]);
-    
-    
 }
+
+exports.updateCommentByID = async (comment_id, inc_votes) => {
+    const checkDbCommentID = await db.query(`SELECT comment_id FROM comments`)
+    const validCommentIDs = [...checkDbCommentID.rows];
+    
+    const response = await db.query(`UPDATE comments SET votes = (votes + $1) WHERE comment_id = $2 RETURNING *;`, [inc_votes, comment_id]);
+    const { rows: [updatedComment] } = await response;
+    return updatedComment;
+}
+
