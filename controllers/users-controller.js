1bd9000093c188bcf924fa12beb5414f9695f59e
clam119 <christopher.lam119@gmail.com>
2022-10-15 04:25:05 +0100

    Add getUserByUsername controller function and import fetchUserByUsername model function.

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,8 +1,18 @@
-const { fetchAllUsers } = require('../models/users-model');
+const { fetchAllUsers, fetchUserByUsername } = require('../models/users-model');
 
 exports.getAllUsers = (req, res, next) => {
     fetchAllUsers()
     .then((usersData) => {
         res.status(200).send(usersData);
     })
+    .catch(next);
+}
+
+exports.getUserByUsername = (req, res, next) => {
+    const { username } = req.params;
+    fetchUserByUsername(username)
+    .then((userData) => {
+        res.status(200).send(userData);
+    })
+    .catch(next);
 }
