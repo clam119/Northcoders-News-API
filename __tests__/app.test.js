0d369e1246e1ef29e016d63693a4649a64e97b83
clam119 <christopher.lam119@gmail.com>
2022-10-16 14:01:15 +0100

    Added Status 201 & 404 tests that will return a 201 & the posted topic if the user specifies a slug and description. But, if the user forgets both or any of the required fields, then they will receive a 404 error specifiying which field they are missing.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -61,6 +61,24 @@ describe('TOPIC TESTS', () => {
         })
       })
 
+      it("Status: 404 - Should respond with a 'Required Fields Missing if the user sends in an empty object that doesn't contain slug/description", async () => {
+        const response = await request(app).post('/api/topics').send({}).expect(404)
+        const {text: msg} = await response;
+        expect(msg).toBe("Required Fields Missing");
+      })
+
+      it("Status: 404 - Should respond with a 'Slug Field Missing if the user sends in an object that doesn't contain slug but contains description", async () => {
+        const response = await request(app).post('/api/topics').send({description: "I missed the slug!"}).expect(404)
+        const {text: msg} = await response;
+        expect(msg).toBe("Slug Field Missing");
+      })
+
+      it("Status: 404 - Should respond with a 'Description Field Missing if the user sends in an object that doesn't contain description but contains slug", async () => {
+        const response = await request(app).post('/api/topics').send({slug: "I missed the description!"}).expect(404)
+        const {text: msg} = await response;
+        expect(msg).toBe("Description Field Missing");
+      })
+
     })
 })
 
--- models/topics-model.js
+++ models/topics-model.js
@@ -7,3 +7,15 @@ exports.fetchAllTopics = () => {
         return topicsData;
     })
 }
+
+exports.postNewTopic = async (slug, description) => {
+
+    //Conditional Logic - Check if required fields existent or not
+    if(slug === undefined) return Promise.reject({status: 400, msg: "Slug Field Missing"});
+    if(description === undefined) return Promise.reject({status: 400, msg: "Description Field Missing"});
+    if(slug && description === undefined) return Promise.reject({status: 400, msg: "Required Fields Missing"});
+
+    const response = await db.query(`INSERT INTO topics (slug, description) VALUES ($1, $2) RETURNING *;`, [slug, description])
+    const { rows: [postedComment] } = await response;
+    return postedComment
+}
