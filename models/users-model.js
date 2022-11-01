e2cfb7c30d6301f14ea5d3543ce367feefdbce2a
clam119 <christopher.lam119@gmail.com>
2022-11-01 22:13:48 +0000

    Add the createNewUser model function that handles errors for invalid data types on the request body or if the user already exists - otherwise will successfully create the user and post it to the database.

--- models/users-model.js
+++ models/users-model.js
@@ -21,3 +21,33 @@ exports.fetchUserByUsername = async (username) => {
     
     return userData;
 }
+
+exports.createNewUser = async (username, name, avatar_url) => {
+    const checkDbUsernames = await db.query(`SELECT * FROM users`);
+    const validUsernames = await checkDbUsernames.rows.map((user) => user.username)
+
+    if(validUsernames.includes(username)) {
+        return Promise.reject({ status: 400, msg: "Username Already Exists" });
+    }
+    
+    if(typeof username !=='string' || typeof name !== 'string' || typeof avatar_url !== 'string'){
+        return Promise.reject({ status: 400, msg: "Invalid Fields Data" })
+    }
+
+    const response = await db.query(`INSERT INTO users(username, name, avatar_url) VALUES ($1, $2, $3) RETURNING *;`, [username, name, avatar_url])
+    const data = await response;
+    const { rows: [userData] } = data;
+
+    return userData;
+}
+
+exports.createCommentByID = (article_id, username, body) => {
+    return db
+      .query(
+        `INSERT INTO comments(author, body, article_id) VALUES ($1, $2, $3) RETURNING *;`,
+        [username, body, article_id]
+      )
+      .then(({ rows: [postedComment] }) => {
+        return postedComment;
+      });
+  };
