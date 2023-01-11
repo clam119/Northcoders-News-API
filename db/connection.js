a5d8d8f03909ef1e4fd1461c50a6c0fcebb45b69
Clam119 <Christopher.lam119@gmail.com>
2023-01-11 00:34:19 +0000

    Moved require dotenv to the top of the file & add max connections of 2.

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,19 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
 const config = ENV === 'production' ? {
         connectionString: process.env.DATABASE_URL,
+        max: 2,
         ssl: {
           rejectUnauthorized: false,
         },
       }
     : {};
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
